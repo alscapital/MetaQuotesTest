f157c418316ba013ec60350973139b6aee543024
alscapital <a.seredin@inbox.ru>
2024-03-13 09:43:26 +0300

    Добавьте файлы проекта.

---
+++ CLogReader.cpp
@@ -0,0 +1,39 @@
+#include "CLogReader.h"
+
+//---
+CLogReader::CLogReader()
+{
+
+}
+//---
+CLogReader::~CLogReader()
+{
+
+}
+//---
+bool CLogReader::Open()
+{
+
+	return true;
+}
+//---
+void CLogReader::Close()
+{
+
+}
+//---
+bool CLogReader::SetFilter(const char* filter)
+{
+
+	return true;
+}
+//---
+bool CLogReader::GetNextLine(char* buf, const int bufsize)
+{
+
+	return true;
+}
+
+
+
+
---
+++ MetaQuotesTest.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+
+#include "CLogReader.h"
+
+/*
+
+
+*/
+
+
+int main(int argc, char* argv[])
+{
+    CLogReader lr;
+
+
+    return 0;
+}
+
